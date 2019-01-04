6e95b40aa7a786972300a1ba343e8eeccb1251d3
ram170 <46366477+ram170@users.noreply.github.com>
2019-01-04 14:40:38 +0530

    Update set1q3.py

--- set1q3.py
+++ set1q3.py
@@ -1,5 +1,11 @@
 s=input()
 if(s=='a' or s=='e' or s=='i' or s=='o' or s=='u' or s=='A' or s=='E' or s=='I' or s=='O' or s=='U'):
     print("Vowel")
-else:
+elif(ord(s)>=97 and ord(s)<=122):
     print("Consonant")
+elif(ord(s)>=65 and ord(s)<=90):    
+    print("Consonant")
+else:
+    print("invalid")
+    
+    
