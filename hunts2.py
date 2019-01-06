51c474fcb134e7f702c9dc06f6d1f94c2d1f3640
ram170 <46366477+ram170@users.noreply.github.com>
2019-01-06 16:54:27 +0530

    Update hunts2.py

--- hunts2.py
+++ hunts2.py
@@ -3,6 +3,9 @@ l=[int(x) for x in input().split()]
 l.sort()
 l.reverse()
 s=str(l[0])
-for i in range(1,n):
+if(l.count(0)==n):
+    print('0')
+else:    
+  for i in range(1,n):
     s=s+str(l[i])
-print(s)    
+  print(s)    
