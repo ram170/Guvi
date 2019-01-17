f134d685fd6d08ffee94af3c7a225d44fc9b71cd
ram170 <46366477+ram170@users.noreply.github.com>
2019-01-17 20:13:09 +0530

    Create pros1q2.py

---
+++ pros1q2.py
@@ -0,0 +1,20 @@
+a,b=map(str,input().split())
+b=int(b)
+l=len(a)-b
+x=1
+for i in range(0,l-1):
+    x=x*10
+#print(x)
+if(b==0):
+    print(a)
+else:    
+ while(1):
+    x=str(x)
+    n=a.count(x)
+    #print(n)
+    if(n==1):
+        print(x)
+        break;
+    else:
+        x=int(x)
+        x+=1
