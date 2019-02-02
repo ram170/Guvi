a42e0043533c52d7a002dedfda9511d7d0bfc824
ram170 <46366477+ram170@users.noreply.github.com>
2019-02-02 19:38:56 +0530

    Create pros2q16.py

---
+++ pros2q16.py
@@ -0,0 +1,16 @@
+n=int(input())
+l=[int(x) for x in input().split()]
+u=1
+t=0
+l.sort()
+for i in range(0,n):
+    if(i==0):
+        t=t+u
+    elif(l[i]==l[i-1]):
+        t=t+u
+    else:
+        u+=1
+        t=t+u
+print(t)        
+        
+    
