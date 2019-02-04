455468f741592139c70f6046450ba5de88f3280c
ram170 <46366477+ram170@users.noreply.github.com>
2019-02-04 00:53:03 -0800

    Update hunts3p28.py

--- hunts3p28.py
+++ hunts3p28.py
@@ -3,13 +3,13 @@ s={'\0'}
 c=[]
 temp=1
 for i in range(0,len(n)):
-    s.insert(n[i])
+    s.add(n[i])
     if(temp!=len(s)):
         c.append(n[i])
         temp=len(s)
 for i in range(0,len(c)):
     if(i!=len(c)-1):
-        print(c[i],end=" ")
+        print(c[i],end="")
     else:
         print(c[i])
         
