c56b4ab957e5178ff780a9445693ac3c31bb6faf
ram170 <46366477+ram170@users.noreply.github.com>
2019-02-04 00:50:13 -0800

    Create hunts3p28.py

---
+++ hunts3p28.py
@@ -0,0 +1,19 @@
+n=list(input())
+s={'\0'}
+c=[]
+temp=1
+for i in range(0,len(n)):
+    s.insert(n[i])
+    if(temp!=len(s)):
+        c.append(n[i])
+        temp=len(s)
+for i in range(0,len(c)):
+    if(i!=len(c)-1):
+        print(c[i],end=" ")
+    else:
+        print(c[i])
+        
+        
+        
+
+
