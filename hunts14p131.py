d94b73ff1c3500d7ec57d1e5f3ac323a2126cee0
ram170 <46366477+ram170@users.noreply.github.com>
2019-02-06 14:40:13 +0530

    Update hunts14p131.py

--- hunts14p131.py
+++ hunts14p131.py
@@ -3,10 +3,10 @@ l=[int(x) for x in input().split()]
 l.sort()
 l.reverse()
 c=[]
-if(i%2==0):
+if(n%2==0):
     f=0
     b=n-1
-    for i in range(0,n/2):
+    for i in range(0,int(n/2)):
       if(f+i!=b-i):    
         c.append(l[f+i])
         c.append(l[b-i])
