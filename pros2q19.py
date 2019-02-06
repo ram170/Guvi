5bb867997b838cedeb23ad01c1de0066ea888761
ram170 <46366477+ram170@users.noreply.github.com>
2019-02-06 18:08:20 +0530

    Update pros2q19.py

--- pros2q19.py
+++ pros2q19.py
@@ -7,8 +7,4 @@ for i in range(0,k):
         q.append(l[j])
     l=[]    
 q.sort()
-for i in range(0,len(q)):
-    if(i==0):
-        print(q[i],end="")
-    else:
-        print(" ",q[i],end="")
+print(*q)
