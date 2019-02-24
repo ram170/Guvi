ae3045682c028a0b19e0b03447665b8000a58eb5
Sriram <46366477+ram170@users.noreply.github.com>
2019-02-24 11:07:09 +0530

    Update pros1q3.py

--- pros1q3.py
+++ pros1q3.py
@@ -1,6 +1,14 @@
 a,b=map(str,input().split())
 u=0
+f=0
 for i in range(0,len(a)):
-    if(a[i]==b[i]):
+   if(len(b)<=i):
+       f=1
+       break;
+   else:
+     if(a[i]==b[i]):
         u+=1
-print(len(b)-u)        
+if(f==1):
+    print(len(a)-u)
+else:    
+    print(len(b)-u)
